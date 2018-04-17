79441e8c375f37261640844204ee6863d294dc28
Marbet Ramon dos Santos <marbet.santos@wdev.com.br>
2018-04-17 11:44:11 -0300

    First Commit

---
+++ NerdSpace_Project/app/src/main/java/br/com/nerdspace/MainActivity.java
@@ -0,0 +1,115 @@
+package br.com.nerdspace;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonSignup;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignin;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() !=null){
+            //profile activity here
+            finish();
+            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+        }
+
+        //initializing views
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewSignin = (TextView) findViewById(R.id.textViewSignin);
+
+        buttonSignup = (Button) findViewById(R.id.buttomRegister);
+
+//        textViewSignup = (TextView) findViewById(R.id.textViewSignUp);
+
+        progressDialog = new ProgressDialog(this);
+
+        //attaching listener to button
+        buttonSignup.setOnClickListener(this);
+        textViewSignin.setOnClickListener(this);
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            //email is empty
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            //stop the function execution further
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            //password is empty
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            //stop the function execution further
+            return;
+        }
+        //if validations are ok
+        //we will first show a progressbar
+
+        progressDialog.setMessage("Registering User...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
+                   @Override
+                   public void onComplete(@NonNull Task<AuthResult> task){
+                       if(task.isSuccessful()){
+                           finish();
+                           startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                       }else {
+                           Toast.makeText(MainActivity.this, "Could not register. Please try again", Toast.LENGTH_SHORT).show();
+                       }
+                       progressDialog.dismiss();
+                   }
+                });
+
+    }
+
+
+    @Override
+    public void onClick(View view){
+
+        if (view == buttonSignup){
+            registerUser();
+        }
+
+        if (view == textViewSignin){
+            //will open login activity here
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+    }
+
+}
+
